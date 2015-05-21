e81fda41d61e9b78c4d97b9155870afb99ccad6a
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-05-21 03:04:54 +0000

    客栈活跃表接口

---
+++ fanqie-dc/trunk/src/main/java/com/fanqie/dc/dto/InnActiveDto.java
@@ -0,0 +1,13 @@
+package com.fanqie.dc.dto;
+
+import com.fanqie.dc.domain.InnActive;
+
+/**
+ * DESC : 活跃客栈报表
+ * @author : 番茄木-ZLin
+ * @data : 2015/5/20
+ * @version: v1.0.0
+ */
+public class InnActiveDto extends InnActive {
+
+}
